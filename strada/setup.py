3b75330af0ca14a4e31d86dc95ff8b1e262a0ab7
huy <huy@huyng.com>
2016-09-29 23:39:06 -0700

    new library called strada

---
+++ strada/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools.extension import Extension
+
+extensions = [
+    Extension(
+        "strada/_caffe_wrapper.cpp",
+        libraries=["caffe"],
+        library_dirs=['../build/lib'],
+    )
+]
+
+
+setup(
+    name='strada',
+    description="Python bindings for caffe, named after the cafe in berkeley",
+    author_email='',
+    version="1.0",
+    packages=['strada'],
+    install_requires=[
+        "Cython>=0.19.2",
+        "numpy>=1.7.1",
+        "scipy>=0.13.2",
+        "scikit-image>=0.9.3",
+        "matplotlib>=1.3.1",
+        "ipython>=3.0.0",
+        "h5py>=2.2.0",
+        "leveldb>=0.191",
+        "networkx>=1.8.1",
+        "nose>=1.3.0",
+        "pandas>=0.12.0",
+        "python-dateutil>=1.4,<2",
+        "protobuf>=2.5.0",
+        "python-gflags>=2.0",
+        "pyyaml>=3.10",
+        "Pillow>=2.3.0",
+        "six>=1.1.0",
+    ],
+    dependency_links=[
+        'http://edge.artifactory.yahoo.com:8000/artifactory/pypi/vision.ioutils/',
+        'http://edge.artifactory.yahoo.com:8000/artifactory/pypi/vision.face/'
+    ],
+    zip_safe=False,
+)
---
+++ strada/strada/__init__.py
@@ -0,0 +1,19 @@
+from .network import Net
+from ._caffe_wrapper import (
+    Layer,
+    SGDSolver,
+    NesterovSolver,
+    AdaGradSolver,
+    RMSPropSolver,
+    AdaDeltaSolver,
+    AdamSolver,
+    set_mode_cpu,
+    set_mode_gpu,
+    set_device,
+    get_solver,
+    layer_type_list,
+    set_random_seed,
+    __version__
+)
+
+from .proto.caffe_pb2 import TRAIN, TEST
